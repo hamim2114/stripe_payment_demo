3f5a384dc1193f3ba518eeb2061a0815ed4a6ada
hamim <hamim2114@gmail.com>
2023-12-03 17:33:03 +0600

    completed

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+require('dotenv').config();
+const Stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const port = 5000;
+
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log('server on port 5000');
+});
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.APP_PASSWORD,
+  },
+});
+
+app.post('/payment', async (req, res) => {
+  let status, error;
+  const { token, amount } = req.body;
+  try {
+    const charge = await Stripe.charges.create({
+      source: token.id,
+      amount,
+      currency: 'usd',
+      receipt_email: token.email,
+    });
+
+    const mailOptions = {
+      from: 'hamim2114@gmail.com',
+      to: token.email,
+      subject: 'Payment Receipt from stripe',
+      html: `
+      <p>Thank you for your purchase of $${amount / 100}.</p>
+      <p>Please find your receipt <a href="${charge.receipt_url}">here</a>.</p>
+    `,
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
+    status = 'success';
+  } catch (err) {
+    console.log(err);
+    status = 'Failure';
+  }
+  res.json({ error, status });
+});
+
+app.use('/', (req, res) => {
+  res.send('Server running on port 5000')
+})

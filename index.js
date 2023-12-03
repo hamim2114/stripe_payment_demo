55544125d21a2dc724ab39b9dc808e6d7d42d5ad
hamim <hamim2114@gmail.com>
2023-12-03 18:07:22 +0600

    completed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({origin: ['http://localhost:4000','https://stripe-payment-demo-client-xfr9.vercel.app'], credentials: true}));
 
 const port = 5000;
 
